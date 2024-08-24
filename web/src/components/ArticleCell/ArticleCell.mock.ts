3f76d3159ea70aab95c544e2252d7d46200fd2ac
실비아헬스 <rlawlsgur620@gmail.com>
2024-08-24 16:47:23 +0900

    init project

---
+++ web/src/components/ArticleCell/ArticleCell.mock.ts
@@ -0,0 +1,4 @@
+// Define your own mock data here:
+export const standard = (/* vars, { ctx, req } */) => ({
+  article: { id: 42, title: 'first post', body: 'body', createdAt: '' },
+})
---
+++ web/src/components/ArticlesCell/ArticlesCell.mock.ts
@@ -0,0 +1,7 @@
+// Define your own mock data here:
+export const standard = (/* vars, { ctx, req } */) => ({
+  articles: [
+    { id: 42, title: 'first post', body: 'body', createdAt: '' },
+    { id: 43, title: 'second post', body: 'body 2', createdAt: '' },
+  ],
+})
